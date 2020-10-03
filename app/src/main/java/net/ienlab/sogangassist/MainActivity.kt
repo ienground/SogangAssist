5d727c47e1dd71e8db3dd7a9e1e887121a099462
IENGROUND <ericano.rhee@gmail.com>
2020-10-04 01:01:37 +0900

    201004_SogangLMSAssist_0.0.1 화면에서 RecyclerView 대충 완료. 이제 정보 수정 해야함

--- app/src/main/java/net/ienlab/sogangassist/MainActivity.kt
+++ app/src/main/java/net/ienlab/sogangassist/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import net.ienlab.sogangassist.decorators.NightModeDecorator
 import net.ienlab.sogangassist.decorators.OneDayDecorator
 import net.ienlab.sogangassist.decorators.SaturdayDecorator
 import net.ienlab.sogangassist.decorators.SundayDecorator
+import java.text.DateFormatSymbols
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -24,7 +26,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         dbHelper = DBHelper(this, dbName, null, dbVersion)
+        val monthFormat = SimpleDateFormat("MMMM", Locale.ENGLISH)
+        month.text = monthFormat.format(Date(System.currentTimeMillis()))
 //        startActivity(Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"))
+
+        val todayWork = dbHelper.getItemAtLastDate(System.currentTimeMillis()).toMutableList().apply {
+            sortBy { it.type }
+        }
+        mainWorkView.adapter = MainWorkAdapter(todayWork)
+        mainWorkView.layoutManager = LinearLayoutManager(this)
 
         val sundayDecorator = SundayDecorator(this)
         val saturdayDecorator = SaturdayDecorator(this)
@@ -35,21 +45,23 @@ class MainActivity : AppCompatActivity() {
         calendarView.topbarVisible = false
         calendarView.addDecorators(sundayDecorator, saturdayDecorator, todayDecorator)
         calendarView.arrowColor = ContextCompat.getColor(this, R.color.black)
+        calendarView.setOnDateChangedListener { _, date, _ ->
+            val work = dbHelper.getItemAtLastDate(date.date.time).toMutableList().apply {
+                sortBy { it.type }
+            }
+            mainWorkView.adapter = MainWorkAdapter(work)
+            mainWorkView.layoutManager = LinearLayoutManager(this)
+        }
+        calendarView.setOnMonthChangedListener { _, date ->
+            month.text = monthFormat.format(date.date)
+        }
 
         if (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES) {
             calendarView.addDecorator(NightModeDecorator(this))
         }
 
         btn_set_today.setOnClickListener {
-            val timeFormat = SimpleDateFormat("yyyy.MM.dd a hh:mm", Locale.KOREA)
-
-            val list = dbHelper.getAllData()
-            for (i in list) {
-                Log.d(TAG, "id:${i.id}, className:${i.className}, timeStamp:${timeFormat.format(Date(i.timeStamp))}")
-                Log.d(TAG, "type:${i.type}, startTime:${timeFormat.format(Date(i.startTime))}, endTime:${timeFormat.format(Date(i.endTime))}")
-                Log.d(TAG, "week:${i.week}, lesson:${i.lesson}, hw_name:${i.homework_name}")
-                Log.d(TAG, "----------------------------------------------------------------------------")
-            }
+            calendarView.setCurrentDate(Date(System.currentTimeMillis()))
         }
     }
 }
---
+++ app/src/main/java/net/ienlab/sogangassist/MainWorkAdapter.kt
@@ -0,0 +1,57 @@
+package net.ienlab.sogangassist
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MainWorkAdapter(internal var mItems: MutableList<LMSClass>) : RecyclerView.Adapter<PostItemHolder>() {
+
+    lateinit var sharedPreferences: SharedPreferences
+    lateinit var context: Context
+
+    // 새로운 뷰 홀더 생성
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostItemHolder {
+        context = parent.context
+        sharedPreferences = context.getSharedPreferences(context.packageName + "_preferences", Context.MODE_PRIVATE)
+        return PostItemHolder(LayoutInflater.from(context).inflate(R.layout.adapter_main_work, parent, false))
+    }
+
+    // View 의 내용을 해당 포지션의 데이터로 바꿉니다.
+    override fun onBindViewHolder(holder: PostItemHolder, position: Int) {
+        Log.d(TAG, "onBindViewHolder")
+
+        holder.class_name.text = mItems[position].className
+        when (mItems[position].type) {
+            LMSType.HOMEWORK -> {
+                holder.icon.setImageResource(R.drawable.ic_assignment)
+                holder.sub_name.text = mItems[position].homework_name
+            }
+
+            LMSType.LESSON, LMSType.SUP_LESSON -> {
+                holder.icon.setImageResource(R.drawable.ic_video)
+                holder.sub_name.text = String.format("%d주 %d차시", mItems[position].week, mItems[position].lesson)
+            }
+        }
+
+
+    }
+
+
+    // 데이터 셋의 크기를 리턴해줍니다.
+    override fun getItemCount(): Int {
+        return mItems.size
+    }
+
+}
+
+class PostItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val icon: ImageView = itemView.findViewById(R.id.icon)
+    val class_name: TextView = itemView.findViewById(R.id.class_name)
+    val sub_name: TextView = itemView.findViewById(R.id.sub_name)
+}
