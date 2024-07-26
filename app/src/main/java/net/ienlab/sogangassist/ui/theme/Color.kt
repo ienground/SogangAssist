fed3dc64714a1951b6b497a928eac18563f15eae
IENGROUND <ericano.rhee@gmail.com>
2024-07-26 16:56:04 +0900

    캘린더 부분 수정

--- app/src/main/java/net/ienlab/sogangassist/ui/theme/Color.kt
+++ app/src/main/java/net/ienlab/sogangassist/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val ColorSunday = Color(0xFFEF5350)
+val ColorSaturday = Color(0xFF448aff)
--- app/src/main/java/net/ienlab/sogangassist/ui/utils/CalendarView.kt
+++ app/src/main/java/net/ienlab/sogangassist/ui/utils/CalendarView.kt
@@ -6,8 +6,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -15,6 +18,9 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -23,14 +29,22 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.util.fastFilterNotNull
 import net.ienlab.sogangassist.Dlog
 import net.ienlab.sogangassist.TAG
 import net.ienlab.sogangassist.ui.theme.AppTheme
+import net.ienlab.sogangassist.ui.theme.ColorSaturday
+import net.ienlab.sogangassist.ui.theme.ColorSunday
 import net.ienlab.sogangassist.ui.utils.Utils.UpdateEffect
 import java.time.DayOfWeek
 import java.time.LocalDate
@@ -89,12 +103,11 @@ fun HorizontalCalendar(
         )
         if (page in pagerState.currentPage - 1..pagerState.currentPage + 1) { // 페이징 성능 개선을 위한 조건문
             CalendarMonthItem(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 20.dp),
                 currentDate = date,
                 selectedDate = selectedDate,
-                onSelectedDate = onSelectedDate
+                onSelectedDate = onSelectedDate,
+                modifier = Modifier
+                    .fillMaxWidth()
             )
         }
     }
@@ -118,7 +131,7 @@ fun CalendarMonthItem(
     val firstDayOfWeek by remember { mutableIntStateOf(currentDate.withDayOfMonth(1).dayOfWeek.value) }
     val lastDayOfWeek by remember { mutableIntStateOf(currentDate.withDayOfMonth(dayLast).dayOfWeek.value)}
     val days by remember { mutableStateOf(IntRange(1, dayLast).toList())}
-    var dayList by remember { mutableStateOf(listOf<List<LocalDate?>>())}
+    var dayList by remember { mutableStateOf(arrayListOf<List<LocalDate?>>())}
 
     LaunchedEffect(currentDate) {
         val _dayList = arrayListOf<LocalDate?>()
@@ -131,49 +144,37 @@ fun CalendarMonthItem(
         for (i in 0 until 6 - lastDayOfWeek % 7) {
             _dayList.add(null)
         }
-        dayList = _dayList.chunked(7)
+        dayList = _dayList.chunked(7) as ArrayList
+        if (dayList.size < 6) dayList.add(listOf(null, null, null, null, null, null, null))
     }
 
     Column(
         modifier = modifier
     ) {
-        DayOfWeekView()
+        DayOfWeekView(
+            modifier = Modifier.fillMaxWidth()
+        )
 
         LazyColumn {
             items(items = dayList, key = { it }) { list ->
                 Row(
                     modifier = Modifier.fillMaxWidth()
-                        .background(if (selectedDate in list) Color.Red else Color.Transparent)
+                        .background(if (selectedDate in list) MaterialTheme.colorScheme.secondaryContainer else Color.Transparent, RoundedCornerShape(16.dp))
                 ) {
                     list.forEach { day ->
-                        Text(
-                            text = day?.dayOfMonth.toString(),
+                        CalendarDay(
+                            date = day,
+                            selected = selectedDate == day,
                             modifier = Modifier
+                                .clip(RoundedCornerShape(16.dp))
                                 .clickable { onSelectedDate(day ?: list.fastFilterNotNull().first() ) }
                                 .weight(1 / 7f, fill = true)
-                                .background(if (selectedDate == day) Color.Green else Color.Transparent)
                         )
                     }
                 }
             }
         }
-
-//        LazyVerticalGrid(
-//            columns = GridCells.Fixed(7)
-//        ) {
-//            for (i in 0 until firstDayOfWeek) {
-//                item {
-//                    Box {}
-//                }
-//            }
-//            items(items = days, key = { it } ) { day ->
-//                val date = currentDate.withDayOfMonth(day)
-//                CalendarDay(date = date)
-//            }
-//        }
-
     }
-
 }
 
 @Composable
@@ -183,8 +184,19 @@ fun DayOfWeekView(
     Row(
         modifier = modifier
     ) {
-        DayOfWeek.entries.forEach { dayOfWeek ->
-            Text(text = (DayOfWeek.of((dayOfWeek.value + 5) % 7 + 1)).getDisplayName(TextStyle.NARROW, Locale.getDefault()))
+        DayOfWeek.entries.forEach { _dayOfWeek ->
+            val dayOfWeek = DayOfWeek.of((_dayOfWeek.value + 5) % 7 + 1)
+            Text(
+                text = dayOfWeek.getDisplayName(TextStyle.NARROW, Locale.getDefault()),
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                color = when (dayOfWeek) {
+                    DayOfWeek.SATURDAY -> ColorSaturday
+                    DayOfWeek.SUNDAY -> ColorSunday
+                    else -> MaterialTheme.colorScheme.onBackground
+                },
+                modifier = Modifier.weight(1f)
+            )
         }
     }
 }
@@ -192,10 +204,23 @@ fun DayOfWeekView(
 @Composable
 fun CalendarDay(
     modifier: Modifier = Modifier,
-    date: LocalDate
+    selected: Boolean,
+    date: LocalDate?
 ) {
     Text(
-        text = date.dayOfMonth.toString()
+        text = date?.dayOfMonth?.toString() ?: "",
+        textAlign = TextAlign.Center,
+        fontSize = if (date?.isEqual(LocalDate.now()) == true) 18.sp else 14.sp,
+        fontWeight = if (date?.isEqual(LocalDate.now()) == true) FontWeight.Bold else FontWeight.Normal,
+        color = when (date?.dayOfWeek) {
+            DayOfWeek.SATURDAY -> ColorSaturday
+            DayOfWeek.SUNDAY -> ColorSunday
+            else -> MaterialTheme.colorScheme.onBackground
+        },
+        modifier = modifier
+            .background(if (selected) MaterialTheme.colorScheme.tertiaryContainer else Color.Transparent, RoundedCornerShape(16.dp))
+            .aspectRatio(1f)
+            .wrapContentHeight(align = Alignment.CenterVertically)
     )
 }
 
